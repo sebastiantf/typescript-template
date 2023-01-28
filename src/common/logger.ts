abc88803395029932d6142a91cbbd80602446de8
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-28 15:25:43 +0530

    feat: ✨ add `logger`

---
+++ src/common/logger.ts
@@ -0,0 +1,13 @@
+import * as winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.printf(
+      (info) => `${info.timestamp}::${info.level}: ${info.message}`
+    )
+  ),
+  transports: [new winston.transports.Console()]
+});
